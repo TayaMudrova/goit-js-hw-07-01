dfc6b25a7cd43aa64b6dc16148663d31a042a999
TayaMudrova <113426294+TayaMudrova@users.noreply.github.com>
2023-02-13 20:27:16 +0200

    fixed prettier

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-	//посилання на контейнер <div class="gallery"></div>
-    const galleryBox = document.querySelector('.gallery');
+//Посилання на контейнер <div class="gallery"></div>
+const galleryBox = document.querySelector(".gallery");
 
-	// додає розмітку галереї в контейнер
-galleryBox.insertAdjacentHTML('beforeend', createImgGalleryMarkup(galleryItems));
+// Додає розмітку галереї в контейнер
+galleryBox.insertAdjacentHTML(
+  "beforeend",
+  createImgGalleryMarkup(galleryItems)
+);
 
-	//Реалізація делегування на div.gallery
-galleryBox.addEventListener('click', onImgGalleryItemClick);
+//Реалізація делегування на div.gallery
+galleryBox.addEventListener("click", onImgGalleryItemClick);
 
-	// створює розмітку галереї за шаблоном з масиву даних galleryItems
-function createImgGalleryMarkup(images) { 
-	return images.map(({ original, preview, description }) => {
-		return `<div class="gallery__item">
+// Створює розмітку галереї за шаблоном з масиву даних galleryItems
+function createImgGalleryMarkup(images) {
+  return images
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
 		<a class="gallery__link" href="${original}">
 		  <img
 			class="gallery__image"
@@ -25,55 +29,53 @@ function createImgGalleryMarkup(images) {
 		  />
 		</a>
 	  </div>`;
-	}).join('');
-
+    })
+    .join("");
 }
 
-	// обробник події click на картинку галереї
+// Обробник події click на картинку галереї
 function onImgGalleryItemClick(event) {
-		// якщо клік не по картинці - вихід з функції
-	if (event.target.nodeName !== 'IMG') {
-		return;
-	}
+  // Якщо клік не по картинці - вихід з функції
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
 
-		//зображення обгорнуте посиланням, по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку
-		// preventDefault забороняє цю поведінку за замовчуванням.
-	event.preventDefault();
+  //Зображення обгорнуте посиланням, по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку
+  // preventDefault забороняє цю поведінку за замовчуванням.
+  event.preventDefault();
 
-		//отримання url великого зображення
-	const urlOriginal = event.target.dataset.source;
+  //Отримання url великого зображення
+  const urlOriginal = event.target.dataset.source;
 
-		//створеня екземпляра бібліотеки basicLightbox
-		// по кліку на картинку відкривається картинка оригільного розміру в окремому модальному вікні
-	const instance = basicLightbox.create(`
+  //Створеня екземпляра бібліотеки basicLightbox
+  // по кліку на картинку відкривається картинка оригільного розміру в окремому модальному вікні
+  const instance = basicLightbox.create(
+    `
     <img src="${urlOriginal}" width="800" height="600">
 	`,
-	  		// об'єкт налаштувань basicLightbox
-		{	
-				// функція, яка виконується перед відображенням Ligtnbox
-			onShow: (instance) => { 
-					// 
-				document.addEventListener('keydown', onEscapeKeydown);
-			},
+    // Об'єкт налаштувань basicLightbox
+    {
+      // Функція, яка виконується перед відображенням Ligtnbox
+      onShow: (instance) => {
+        //
+        document.addEventListener("keydown", onEscapeKeydown);
+      },
 
-				// функція, яка виконується перед закриттям Ligtnbox
-			onClose: (instance) => {
-				// знімаємо прослуховування клавіатури, перед закриттям  модального вікна
-				document.removeEventListener('keydown', onEscapeKeydown);
-			}
+      // Функція, яка виконується перед закриттям Ligtnbox
+      onClose: (instance) => {
+        // Знімаємо прослуховування клавіатури, перед закриттям  модального вікна
+        document.removeEventListener("keydown", onEscapeKeydown);
+      },
+    }
+  );
 
-		}
-	)
-	
-	instance.show();
+  instance.show();
 
-	
-	function onEscapeKeydown(event) {
-		console.log(event.code);
-		if (event.code === 'Escape') {
-
-				// close() - метод бібліотеки basicLightbox, закриває модальне вікно	
-			instance.close();
-		}	
-	}
+  function onEscapeKeydown(event) {
+    console.log(event.code);
+    if (event.code === "Escape") {
+      // close() - метод бібліотеки basicLightbox, закриває модальне вікно
+      instance.close();
+    }
+  }
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,28 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
+//Посилання на контейнер <div class="gallery"></div>
+const galleryBox = document.querySelector(".gallery");
 
-	//посилання на контейнер <div class="gallery"></div>
-    const galleryBox = document.querySelector('.gallery');
+// Додає розмітку галереї в контейнер
+galleryBox.insertAdjacentHTML(
+  "beforeend",
+  createImgGalleryMarkup(galleryItems)
+);
 
-	// додає розмітку галереї в контейнер
-galleryBox.insertAdjacentHTML('beforeend', createImgGalleryMarkup(galleryItems));
-
-	// створює розмітку галереї за шаблоном з масиву даних galleryItems
-function createImgGalleryMarkup(images) { 
-	return images.map(({ original, preview, description }) => {
-		return `<a class="gallery__item" href="${original}">
+// Створює розмітку галереї за шаблоном з масиву даних galleryItems
+function createImgGalleryMarkup(images) {
+  return images
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
 		<img class="gallery__image" src="${preview}" alt="${description}" />
 	  </a>`;
-	}).join('');
-
+    })
+    .join("");
 }
-	// initializing of SimpleLightbox instance
-const lightbox = new SimpleLightbox('.gallery a', {
-		// get the caption from given attribute
-	captionsData: 'alt',
-		// adds a delay before the caption shows
-	captionDelay: 250,
-	});
+// Іnitializing of SimpleLightbox instance
+const lightbox = new SimpleLightbox(".gallery a", {
+  // Get the caption from given attribute
+  captionsData: "alt",
+  // Adds a delay before the caption shows
+  captionDelay: 250,
+});
